9697f73306c121d04f9f2c9c9d2d4ec58ad6d81a
mohamedwardi068 <mohamedwardi068@gmail.com>
2025-11-29 12:28:01 +0100

    deployFinal

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { AuthProvider } from "./context/authcontext"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
 
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
 
   </React.StrictMode>
 )
